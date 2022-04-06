fbb0958bcf241b21ea8474d874684470abb85f84
Owen Mak <owdevel@gmail.com>
2022-04-06 18:20:50 +1000

    Successfully implemented IMU data in testing listener.py

--- src/testing/scripts/listener.py
+++ src/testing/scripts/listener.py
@@ -2,6 +2,7 @@
 
 import rospy
 import mavros_msgs.msg
+import mavros_msgs.srv
 import sensor_msgs.msg
 
 
@@ -11,6 +12,14 @@ def callback(data):
 
 def main():
     #rospy.Subscriber('mavros/state', mavros_msgs.msg.State, callback)
+
+    # Set Stream Rate of data calls for mavros
+    set_stream_rate = rospy.ServiceProxy('/mavros/set_stream_rate',
+                                         mavros_msgs.srv.StreamRate)
+
+    # Stream 0 (maybe linked to modes?), 10Hz, Enable
+    set_stream_rate(0, 10, 1)
+
     rospy.Subscriber('mavros/imu/data', sensor_msgs.msg.Imu, callback)
 
 
