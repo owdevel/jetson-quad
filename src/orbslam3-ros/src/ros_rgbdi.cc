1ce119f0c6ef2b418bf69ff05616e5453de45cef
Owen M <owdevel@gmail.com>
2022-09-21 18:49:10 +1000

    Added transform from orbslam camera to base_link

--- src/orbslam3-ros/src/ros_rgbdi.cc
+++ src/orbslam3-ros/src/ros_rgbdi.cc
@@ -1,36 +1,35 @@
 /**
-* This file is part of ORB-SLAM3
-*
-* Copyright (C) 2017-2021 Carlos Campos, Richard Elvira, Juan J. Gómez Rodríguez, José M.M. Montiel and Juan D. Tardós, University of Zaragoza.
-* Copyright (C) 2014-2016 Raúl Mur-Artal, José M.M. Montiel and Juan D. Tardós, University of Zaragoza.
-*
-* ORB-SLAM3 is free software: you can redistribute it and/or modify it under the terms of the GNU General Public
-* License as published by the Free Software Foundation, either version 3 of the License, or
-* (at your option) any later version.
-*
-* ORB-SLAM3 is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even
-* the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-* GNU General Public License for more details.
-*
-* You should have received a copy of the GNU General Public License along with ORB-SLAM3.
-* If not, see <http://www.gnu.org/licenses/>.
-*/
+ * This file is part of ORB-SLAM3
+ *
+ * Copyright (C) 2017-2021 Carlos Campos, Richard Elvira, Juan J. Gómez Rodríguez, José M.M. Montiel and Juan D. Tardós, University of Zaragoza.
+ * Copyright (C) 2014-2016 Raúl Mur-Artal, José M.M. Montiel and Juan D. Tardós, University of Zaragoza.
+ *
+ * ORB-SLAM3 is free software: you can redistribute it and/or modify it under the terms of the GNU General Public
+ * License as published by the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * ORB-SLAM3 is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even
+ * the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with ORB-SLAM3.
+ * If not, see <http://www.gnu.org/licenses/>.
+ */
 
+#include <iostream>
+#include <algorithm>
+#include <fstream>
+#include <chrono>
+#include <mutex>
+#include <queue>
 
-#include<iostream>
-#include<algorithm>
-#include<fstream>
-#include<chrono>
-#include<mutex>
-#include<queue>
-
-#include<ros/ros.h>
+#include <ros/ros.h>
 #include <cv_bridge/cv_bridge.h>
 #include <message_filters/subscriber.h>
 #include <message_filters/time_synchronizer.h>
 #include <message_filters/sync_policies/approximate_time.h>
 
-#include<opencv2/core/core.hpp>
+#include <opencv2/core/core.hpp>
 
 #include "System.h"
 
@@ -38,6 +37,7 @@
 #include <sensor_msgs/Imu.h>
 #include <Eigen/Geometry>
 #include <tf/transform_broadcaster.h>
+#include <tf/transform_listener.h>
 
 using namespace std;
 
@@ -54,16 +54,16 @@ public:
 class ImageGrabber
 {
 public:
-    ImageGrabber(ORB_SLAM3::System* pSLAM, ImuGrabber *pImuGb):mpSLAM(pSLAM), mpImuGb(pImuGb){}
+    ImageGrabber(ORB_SLAM3::System *pSLAM, ImuGrabber *pImuGb) : mpSLAM(pSLAM), mpImuGb(pImuGb), listener(ros::Duration(10)) {}
 
-    void GrabRGBD(const sensor_msgs::ImageConstPtr& msgRGB,const sensor_msgs::ImageConstPtr& msgD);
+    void GrabRGBD(const sensor_msgs::ImageConstPtr &msgRGB, const sensor_msgs::ImageConstPtr &msgD);
 
-    ORB_SLAM3::System* mpSLAM;
+    ORB_SLAM3::System *mpSLAM;
     ImuGrabber *mpImuGb;
+    tf::TransformListener listener;
 
     ros::Publisher pub;
     ros::Publisher depth;
-
 };
 
 int main(int argc, char **argv)
@@ -71,26 +71,27 @@ int main(int argc, char **argv)
     ros::init(argc, argv, "RGBD");
     ros::start();
 
-    if(argc != 4)
+    if (argc != 4)
     {
-        cerr << endl << "Usage: rosrun ORB_SLAM3 RGBD path_to_vocabulary path_to_settings gui" << endl;        
+        cerr << endl
+             << "Usage: rosrun ORB_SLAM3 RGBD path_to_vocabulary path_to_settings gui" << endl;
         ros::shutdown();
         return 1;
-    }    
+    }
 
     // Create SLAM system. It initializes all system threads and gets ready to process frames.
 
     const string tr = "true";
     const bool bUseViewer = tr.compare(argv[3]) == 0;
 
-    ORB_SLAM3::System SLAM(argv[1],argv[2],ORB_SLAM3::System::RGBD,bUseViewer);
+    ORB_SLAM3::System SLAM(argv[1], argv[2], ORB_SLAM3::System::RGBD, bUseViewer);
 
     ImuGrabber imugb;
     ImageGrabber igb(&SLAM, &imugb);
 
     ros::NodeHandle nh;
 
-    ros::Subscriber sub_imu = nh.subscribe("/camera/imu", 1000, &ImuGrabber::GrabImu, &imugb); 
+    ros::Subscriber sub_imu = nh.subscribe("/camera/imu", 1000, &ImuGrabber::GrabImu, &imugb);
 
     igb.pub = nh.advertise<geometry_msgs::PoseStamped>("/orbslam_pose", 100, true);
     igb.depth = nh.advertise<sensor_msgs::Image>("/orbslam_depth", 100, true);
@@ -98,8 +99,8 @@ int main(int argc, char **argv)
     message_filters::Subscriber<sensor_msgs::Image> rgb_sub(nh, "/camera/color/image_raw", 100);
     message_filters::Subscriber<sensor_msgs::Image> depth_sub(nh, "/camera/depth/image_rect_raw", 100);
     typedef message_filters::sync_policies::ApproximateTime<sensor_msgs::Image, sensor_msgs::Image> sync_pol;
-    message_filters::Synchronizer<sync_pol> sync(sync_pol(10), rgb_sub,depth_sub);
-    sync.registerCallback(boost::bind(&ImageGrabber::GrabRGBD,&igb,_1,_2));
+    message_filters::Synchronizer<sync_pol> sync(sync_pol(10), rgb_sub, depth_sub);
+    sync.registerCallback(boost::bind(&ImageGrabber::GrabRGBD, &igb, _1, _2));
 
     ros::spin();
 
@@ -114,7 +115,7 @@ int main(int argc, char **argv)
     return 0;
 }
 
-void ImageGrabber::GrabRGBD(const sensor_msgs::ImageConstPtr& msgRGB,const sensor_msgs::ImageConstPtr& msgD)
+void ImageGrabber::GrabRGBD(const sensor_msgs::ImageConstPtr &msgRGB, const sensor_msgs::ImageConstPtr &msgD)
 {
     // Copy the ros image message to cv::Mat.
     cv_bridge::CvImageConstPtr cv_ptrRGB;
@@ -122,7 +123,7 @@ void ImageGrabber::GrabRGBD(const sensor_msgs::ImageConstPtr& msgRGB,const senso
     {
         cv_ptrRGB = cv_bridge::toCvShare(msgRGB);
     }
-    catch (cv_bridge::Exception& e)
+    catch (cv_bridge::Exception &e)
     {
         ROS_ERROR("cv_bridge exception: %s", e.what());
         return;
@@ -133,85 +134,82 @@ void ImageGrabber::GrabRGBD(const sensor_msgs::ImageConstPtr& msgRGB,const senso
     {
         cv_ptrD = cv_bridge::toCvShare(msgD);
     }
-    catch (cv_bridge::Exception& e)
+    catch (cv_bridge::Exception &e)
     {
         ROS_ERROR("cv_bridge exception: %s", e.what());
         return;
     }
 
-
     double tIm = msgRGB->header.stamp.toSec();
 
     vector<ORB_SLAM3::IMU::Point> vImuMeas;
     mpImuGb->mBufMutex.lock();
-    if(!mpImuGb->imuBuf.empty())
+    if (!mpImuGb->imuBuf.empty())
     {
-      // Load imu measurements from buffer
-      vImuMeas.clear();
-      while(!mpImuGb->imuBuf.empty() && mpImuGb->imuBuf.front()->header.stamp.toSec()<=tIm)
-      {
-        double t = mpImuGb->imuBuf.front()->header.stamp.toSec();
-        cv::Point3f acc(mpImuGb->imuBuf.front()->linear_acceleration.x, mpImuGb->imuBuf.front()->linear_acceleration.y, mpImuGb->imuBuf.front()->linear_acceleration.z);
-        cv::Point3f gyr(mpImuGb->imuBuf.front()->angular_velocity.x, mpImuGb->imuBuf.front()->angular_velocity.y, mpImuGb->imuBuf.front()->angular_velocity.z);
-        vImuMeas.push_back(ORB_SLAM3::IMU::Point(acc,gyr,t));
-        mpImuGb->imuBuf.pop();
-      }
+        // Load imu measurements from buffer
+        vImuMeas.clear();
+        while (!mpImuGb->imuBuf.empty() && mpImuGb->imuBuf.front()->header.stamp.toSec() <= tIm)
+        {
+            double t = mpImuGb->imuBuf.front()->header.stamp.toSec();
+            cv::Point3f acc(mpImuGb->imuBuf.front()->linear_acceleration.x, mpImuGb->imuBuf.front()->linear_acceleration.y, mpImuGb->imuBuf.front()->linear_acceleration.z);
+            cv::Point3f gyr(mpImuGb->imuBuf.front()->angular_velocity.x, mpImuGb->imuBuf.front()->angular_velocity.y, mpImuGb->imuBuf.front()->angular_velocity.z);
+            vImuMeas.push_back(ORB_SLAM3::IMU::Point(acc, gyr, t));
+            mpImuGb->imuBuf.pop();
+        }
     }
     mpImuGb->mBufMutex.unlock();
 
+    Sophus::SE3f pose = mpSLAM->TrackRGBD(cv_ptrRGB->image, cv_ptrD->image, cv_ptrRGB->header.stamp.toSec(), vImuMeas);
 
-
-    Sophus::SE3f pose = mpSLAM->TrackRGBD(cv_ptrRGB->image,cv_ptrD->image,cv_ptrRGB->header.stamp.toSec(), vImuMeas);
-
-    ROS_INFO("Pose: X: %f, Y:%f, Z:%f", pose.translation()(0), pose.translation()(1), pose.translation()(2));
+    // ROS_INFO("Pose: X: %f, Y:%f, Z:%f", pose.translation()(0), pose.translation()(1), pose.translation()(2));
     Eigen::Vector3f translation = pose.translation();
     Eigen::Quaternionf quaternion = pose.unit_quaternion();
 
+    try
+    {
+        geometry_msgs::PoseStamped cameraPose;
+        cameraPose.pose.position.x = translation.x();
+        cameraPose.pose.position.y = translation.z();
+        cameraPose.pose.position.z = translation.y();
+        cameraPose.pose.orientation.w = quaternion.w();
+        cameraPose.pose.orientation.x = quaternion.x();
+        cameraPose.pose.orientation.y = quaternion.z();
+        cameraPose.pose.orientation.z = quaternion.y();
 
-    geometry_msgs::PoseStamped poseStamp;
-    poseStamp.pose.position.x = translation.x();
-    poseStamp.pose.position.y = translation.z();
-    poseStamp.pose.position.z = translation.y();
-    poseStamp.pose.orientation.w = quaternion.w();
-    poseStamp.pose.orientation.x = quaternion.x();
-    poseStamp.pose.orientation.y = quaternion.z();
-    poseStamp.pose.orientation.z = quaternion.y();
+        cameraPose.header.stamp = msgD->header.stamp;
+        cameraPose.header.frame_id = msgD->header.frame_id;
+        // pub.publish(cameraPose);
 
-    poseStamp.header.stamp = msgD->header.stamp;
-    poseStamp.header.frame_id = msgD->header.frame_id;
+        geometry_msgs::PoseStamped basePose;        
+        listener.transformPose("base_link", cameraPose, basePose);
 
-    static tf::TransformBroadcaster br;
-    tf::Transform transform;
-    transform.setOrigin(tf::Vector3(poseStamp.pose.position.x, poseStamp.pose.position.y, poseStamp.pose.position.z));
-    tf::Quaternion q;
-    q.setW(poseStamp.pose.orientation.w);
-    q.setX(poseStamp.pose.orientation.x);
-    q.setY(poseStamp.pose.orientation.y);
-    q.setZ(poseStamp.pose.orientation.z);
-    transform.setRotation(q);
 
-    tf::StampedTransform stf;
-    stf.setData(transform);
-    stf.stamp_ = msgD->header.stamp;
-    stf.frame_id_ = "map";
-    stf.child_frame_id_ = "camera_link";
-    // stf.frame_id_ = msgD->header.frame_id;
-    // stf.child_frame_id_ = "map";
+        tf::StampedTransform stf;
+        stf.stamp_ = basePose.header.stamp;
+        stf.setOrigin(tf::Vector3(basePose.pose.position.x, basePose.pose.position.y, basePose.pose.position.z));
+        stf.setRotation(tf::Quaternion(basePose.pose.orientation.x, basePose.pose.orientation.y, basePose.pose.orientation.z, basePose.pose.orientation.w));
+        stf.frame_id_ = "map";
+        stf.child_frame_id_ = "base_link";
 
-    br.sendTransform(stf);
+        ROS_INFO("TransformPose: X: %f, Y:%f, Z:%f", basePose.pose.position.x, basePose.pose.position.y, basePose.pose.position.z);
 
-    pub.publish(poseStamp);
 
-    depth.publish(msgD);
+        static tf::TransformBroadcaster br;
+        br.sendTransform(stf);
 
+
+        depth.publish(msgD);
+    }
+    catch (tf::TransformException ex)
+    {
+        ROS_ERROR("%s", ex.what());
+    }
 }
 
 void ImuGrabber::GrabImu(const sensor_msgs::ImuConstPtr &imu_msg)
 {
-  mBufMutex.lock();
-  imuBuf.push(imu_msg);
-  mBufMutex.unlock();
-  return;
+    mBufMutex.lock();
+    imuBuf.push(imu_msg);
+    mBufMutex.unlock();
+    return;
 }
-
-
