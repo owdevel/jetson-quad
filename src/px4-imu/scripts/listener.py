3878a86106b60ee25ed1f3e88e5540f68ee27081
Owen M <owdevel@gmail.com>
2022-11-05 23:16:43 +1100

    Kalibr

--- src/px4-imu/scripts/listener.py
+++ src/px4-imu/scripts/listener.py
@@ -14,13 +14,14 @@ def main():
     print('Subscribing to State')
     rospy.Subscriber('mavros/state', mavros_msgs.msg.State, callback)
 
-    print('Setting stream rate')
-    # Set Stream Rate of data calls for mavros
-    set_stream_rate = rospy.ServiceProxy('/mavros/set_stream_rate',
-                                         mavros_msgs.srv.StreamRate)
+    # print('Setting stream rate')
+    # # Set Stream Rate of data calls for mavros
+    # set_stream_rate = rospy.ServiceProxy('/mavros/set_stream_rate',
+    #                                      mavros_msgs.srv.StreamRate)
 
-    # Stream 0 (maybe linked to modes?), 10Hz, Enable
-    set_stream_rate(0, 200, 1)
+    # # Stream 0 (maybe linked to modes?), 10Hz, Enable
+    # set_stream_rate(0, 200, 1)
+    set_message_interval = rospy.ServiceProxy('/mavros/set_message_interval', mavros_msgs.srv.MessageInterval)
 
     print('Subscribing to IMU')
     rospy.Subscriber('mavros/imu/data', sensor_msgs.msg.Imu, callback)
